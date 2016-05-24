d4a6a5a65b831ec2b454950787e1ccb93c3b781f
David Whitaker <damwhit@gmail.com>
2016-05-23 22:26:19 -0600

    add first component

---
+++ scripts/main.js
@@ -0,0 +1,13 @@
+var React = require('react');
+var ReactDom = require('react-dom');
+
+var StorePicker = React.createClass({
+
+  render : function() {
+    return (
+      <p>hello</p>
+    )
+  }
+});
+
+ReactDom.render(<StorePicker/>, document.querySelector('#main'));
