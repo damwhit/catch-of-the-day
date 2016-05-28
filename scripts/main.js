a60d7cf11bd38d7829807b6d595f685b430fd447
David Whitaker <damwhit@gmail.com>
2016-05-28 16:54:15 -0600

    add react router
    installed react router and require it
    installed and implement create browser history for router
    implemented not found 404

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,12 @@
 var React = require('react');
 var ReactDom = require('react-dom');
 
+var ReactRouter = require('react-router')
+var Router = ReactRouter.Router;
+var Route = ReactRouter.Route;
+var Navigation = ReactRouter.Navigation;
+var createBrowserHistory = require('history/lib/createBrowserHistory');
+
 /*
   App
 */
@@ -83,4 +89,24 @@ var StorePicker = React.createClass({
   }
 });
 
-ReactDom.render(<App/>, document.querySelector('#main'));
+/*
+Not Found
+*/
+var NotFound = React.createClass({
+  render : function() {
+    return <h1>Not Found!</h1>
+  }
+})
+/*
+Routes
+*/
+
+var routes = (
+  <Router history={createBrowserHistory()}>
+    <Route path="/" component={StorePicker}/>
+    <Route path="/store/:storeId" component={App}/>
+    <Route path='*' component={NotFound}></Route>
+  </Router>
+)
+
+ReactDom.render(routes, document.querySelector('#main'));
